fb29d738409a9fd54b40ac9f5e86b86ae115b7b0
pafim37 <astropawelpiatek@gmail.com>
2022-01-23 17:32:29 +0100

    add ef6 for observations

---
+++ src/Sas.Db.ObservationDatabase/Data/ObservationDbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using Sas.Db.ObservationDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sas.Db.ObservationDatabase.Data
+{
+    internal class ObservationDbInitializer : CreateDatabaseIfNotExists<ObservationDb>
+    {
+        protected override void Seed(ObservationDb context)
+        {
+            IList<Observatory> observatories = new List<Observatory>();
+            IList<Observation> observations = new List<Observation>();
+
+            Observatory Cracow = new Observatory { Id = 1, Name = "Cracow", Latitude = ChangeDegToRad(50.06143), Longitude = ChangeDegToRad(19.93658) };
+
+            observatories.Add(Cracow);
+            observatories.Add(new Observatory { Id = 1, Name = "Greenwich", Latitude = ChangeDegToRad(51.47781), Longitude = ChangeDegToRad(0.00148) });
+            observatories.Add(new Observatory { Id = 1, Name = "New York Ford", Latitude = ChangeDegToRad(42.41753), Longitude = ChangeDegToRad(-76.49407) });
+
+            context.Observatories.AddRange(observatories);
+
+            observations.Add(new Observation { Id = 1, ObjectName = "Moon", ObservatoryId = 1, CreatedOn = new DateTime(2013, 8, 21, 19, 0, 0), Azimuth = ChangeDegToRad(102.15467), Altitude = ChangeDegToRad(3.343), Distance = 367273905, Observatory = Cracow });
+
+            context.Observations.AddRange(observations);
+            base.Seed(context);
+        }
+
+        private double ChangeDegToRad(double deg)
+        {
+            return Math.PI * deg / 180;
+        }
+    }
+}
---
+++ src/Sas.Db.ObservationDatabase/Models/Observatory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sas.Db.ObservationDatabase.Models
+{
+    public class Observatory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        
+        [ForeignKey("Observations")]
+        public ICollection<Observation> Observations { get; set;}
+    }
+}
